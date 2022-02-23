92da9ab61ad2c0176e8504c09456d6e2da5877ca
ChuTianshu717 <cts717cs@gmail.com>
2022-02-23 23:24:37 +0000

    Complete the exercises in Chapter 7 and pass the test

--- rango/models.py
+++ rango/models.py
@@ -3,7 +3,8 @@ from django.template.defaultfilters import slugify
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    MAX_LENGTH = 128
+    name = models.CharField(max_length=MAX_LENGTH, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
@@ -21,7 +22,7 @@ class Category(models.Model):
 
 class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=Category.MAX_LENGTH)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
