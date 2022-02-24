15f1855f089570a03ee1f06bce57fd18b4b36994
ChuTianshu717 <cts717cs@gmail.com>
2022-02-24 01:02:11 +0000

    Complete the exercises in Chapter 10 and pass the test

--- rango/views.py
+++ rango/views.py
@@ -10,6 +10,28 @@ from rango.forms import PageForm
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
 
 
 def index(request):
@@ -19,12 +41,15 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+    visitor_cookie_handler(request)
     return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
-    # return HttpResponse("Rango says here is the about page.<br/><a href='/rango/'>Index</a>")
-    return render(request, 'rango/about.html', {})
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'rango/about.html', context=context_dict)
 
 
 def show_category(request, category_name_slug):
@@ -54,7 +79,6 @@ def add_category(request):
         else:
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
-
 
 
 def add_page(request, category_name_slug):
@@ -141,7 +165,6 @@ def user_login(request):
         return render(request, 'rango/login.html')
 
 
-
 def restricted(request):
     if not request.user.is_authenticated:
         return redirect(reverse('rango:login'))
@@ -152,3 +175,6 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+
+
